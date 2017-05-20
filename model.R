2ac2d180a3ddd26c7e1c26ddcb2bb4da6760da2b
Arni Magnusson <arnima@hafro.is>
2017-05-20 17:35:12 +0200

    Run model

--- model.R
+++ model.R
@@ -1,1 +1,19 @@
 ## Run analysis, write model results
+
+require(icesTAF, quietly=TRUE)
+
+mkdir("model")
+
+ftp <- "https://stockassessment.org/datadisk/stockassessment/userdirs/user3/nscod16-ass02/"
+
+download.file(paste0(ftp,"run/model.cfg"), "model/model.cfg", quiet=TRUE)
+download.file(paste0(ftp,"run/sam.pin"), "model/sam.pin", quiet=TRUE)
+
+sam <- if(.Platform$OS.type == "unix") "sam" else "sam.exe"
+download.file(paste0(ftp,"/run/",sam), paste0("model/",sam), quiet=TRUE)
+Sys.chmod(paste0("model/", sam))
+
+cp("input/sam.dat", "model")
+
+setwd("model")
+system("sam -nr 2 -noinit")
