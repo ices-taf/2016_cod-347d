af859e32570275b122f45774a3e4a57e99b73b56
Arni Magnusson <arnima@hafro.is>
2017-10-23 22:08:27 +0200

    Upload model config and initial params

--- _model/upload.R
+++ _model/upload.R
@@ -1,11 +1,13 @@
 ## Upload model executables to TAF database
 
-## Before: sam, sam.exe (user dir)
-## After:  sam, sam.exe (TAF database)
+## Before: model.cfg, sam, sam.exe, sam.pin (user dir)
+## After:  model.cfg, sam, sam.exe, sam.pin (TAF database)
 
 library(icesTAF)
 
 owd <- setwd("d:/projects/ices-taf/ftp/wgnssk/2016/cod-347d/model")
 upload("2016_cod-347d", "model", "sam.exe")
 upload("2016_cod-347d", "model", "sam")
+upload("2016_cod-347d", "model", "model.cfg")
+upload("2016_cod-347d", "model", "sam.pin")
 setwd(owd)
