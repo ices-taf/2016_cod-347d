309d65268116cda999766de68ac0407973c01877
Arni Magnusson <arnima@hafro.is>
2017-11-01 07:08:09 +0100

    Plot B and F

---
+++ report_plots.R
@@ -0,0 +1,26 @@
+## Prepare plots for report
+
+## Before: summary.csv (output)
+## After:  biomass.png, f_mortality.png (report)
+
+library(icesTAF)
+library(areaplot)
+
+mkdir("report")
+
+## Change unit
+summary <- read.taf("output/summary.csv")
+x <- div(summary, "SSB", grep=TRUE)
+
+## Plots
+tafpng("biomass")
+confplot(x[c("Year","SSB_lo","SSB_hi")], ylim=lim(x$SSB_hi), yaxs="i",
+         ylab="SSB (1000 t)", main="Biomass")
+lines(x$Year, x$SSB)
+dev.off()
+
+tafpng("f_mortality")
+confplot(x[c("Year","Fbar_lo","Fbar_hi")], ylim=lim(x$Fbar_hi), yaxs="i",
+         ylab="Fbar (2-4)", main="Fishing mortality")
+lines(x$Year, x$Fbar)
+dev.off()
