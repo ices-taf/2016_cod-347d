93c8a8767b0121df7b50624b7fa55fca6454fc5e
Arni Magnusson <arnima@hafro.is>
2018-12-15 09:09:46 +0100

    Get model and config files from 'bootstrap' folder

--- model.R
+++ model.R
@@ -1,18 +1,22 @@
 ## Run analysis, write model results
 
-## Before: model.cfg, sam, sam.pin (begin/initial/model), sam.dat (input)
+## Before: sam (bootstrap/software), model.cfg, sam.pin (bootstrap/config),
+##         sam.dat (data)
 ## After:  confclone.log, model.cfg, sam.cor, sam.par, sam.rep, sam.res (model)
 
 library(icesTAF)
 
 mkdir("model")
 
-sam <- if(.Platform$OS.type == "unix") "sam" else "sam.exe"
-cp(file.path("begin/initial/model",sam), "model")
-cp("begin/initial/model/model.cfg", "model")
-cp("begin/initial/model/sam.pin", "model")
+## Get model executable
+exefile <- if(os.unix()) "sam" else "sam.exe"
+taf.unzip("bootstrap/software/sam.zip", files=exefile, exdir="model")
 
-cp("input/sam.dat", "model")
+## Get model configuration
+cp("bootstrap/config/*", "model")
+
+## Get model input file
+cp("data/sam.dat", "model")
 
 setwd("model")
 system("./sam -nr 2 -noinit")
