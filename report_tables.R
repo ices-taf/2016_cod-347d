7dcaeb474f7a2df511c5939e5be2830ab1423432
Arni Magnusson <arnima@hafro.is>
2019-04-16 15:49:56 +0200

    Use write.taf(dir) notation

--- report_tables.R
+++ report_tables.R
@@ -21,7 +21,7 @@ latage <- latage[c("Year",as.character(1:11))]
 names(latage)[ncol(latage)] <- "+gp"
 latage <- round(latage)
 latage <- tt(latage, TRUE)
-write.taf(latage, "report/latage.csv")
+write.taf(latage, dir="report")
 
 ## datage (trim age, round, transpose)
 datage <- read.taf("data/datage_full.csv")
@@ -30,7 +30,7 @@ datage <- datage[c("Year",as.character(1:11))]
 names(datage)[ncol(datage)] <- "+gp"
 datage <- round(datage)
 datage <- tt(datage, TRUE)
-write.taf(datage, "report/datage.csv")
+write.taf(datage, dir="report")
 
 ## catage (trim age, round, transpose)
 catage <- read.taf("data/catage_full.csv")
@@ -39,7 +39,7 @@ catage <- catage[c("Year",as.character(1:11))]
 names(catage)[ncol(catage)] <- "+gp"
 catage <- round(catage)
 catage <- tt(catage, TRUE)
-write.taf(catage, "report/catage.csv")
+write.taf(catage, dir="report")
 
 ## wlandings (trim age, round, transpose)
 wlandings <- read.taf("data/wlandings_full.csv")
@@ -51,7 +51,7 @@ wlandings <- wlandings[c("Year",as.character(1:11))]
 names(wlandings)[ncol(wlandings)] <- "+gp"
 wlandings <- round(wlandings, 3)
 wlandings <- tt(wlandings, TRUE)
-write.taf(wlandings, "report/wlandings.csv")
+write.taf(wlandings, dir="report")
 
 ## wdiscards (trim age, round, transpose)
 wdiscards <- read.taf("data/wdiscards_full.csv")
@@ -63,7 +63,7 @@ wdiscards <- wdiscards[c("Year",as.character(1:11))]
 names(wdiscards)[ncol(wdiscards)] <- "+gp"
 wdiscards <- round(wdiscards, 3)
 wdiscards <- tt(wdiscards, TRUE)
-write.taf(wdiscards, "report/wdiscards.csv")
+write.taf(wdiscards, dir="report")
 
 ## wcatch (trim age, round, transpose)
 wcatch <- read.taf("data/wcatch_full.csv")
@@ -75,49 +75,49 @@ wcatch <- wcatch[c("Year",as.character(1:11))]
 names(wcatch)[ncol(wcatch)] <- "+gp"
 wcatch <- round(wcatch, 3)
 wcatch <- tt(wcatch, TRUE)
-write.taf(wcatch, "report/wcatch.csv")
+write.taf(wcatch, dir="report")
 
-## catch_sop (round)
-catch_sop <- read.taf("data/catch_sop.csv")
-catch_sop <- round(catch_sop)
-write.taf(catch_sop, "report/catch_sop.csv")
+## catch.sop (round)
+catch.sop <- read.taf("data/catch_sop.csv")
+catch.sop <- round(catch.sop)
+write.taf(catch.sop, dir="report")
 
 ## maturity (round)
 maturity <- read.taf("data/maturity.csv")
 maturity <- round(maturity, 3)
-write.taf(maturity, "report/maturity.csv")
+write.taf(maturity, dir="report")
 
 ## natmort (round)
 natmort <- read.taf("data/natmort.csv")
 natmort <- round(natmort, 3)
-write.taf(natmort, "report/natmort.csv")
+write.taf(natmort, dir="report")
 
 ## ibts1 (round)
 ibts1 <- read.taf("data/ibts1.csv")
 ibts1 <- round(ibts1, 2)
-write.taf(ibts1, "report/ibts1.csv")
+write.taf(ibts1, dir="report")
 
 ## ibts3 (round)
 ibts3 <- read.taf("data/ibts3.csv")
 ibts3 <- round(ibts3, 2)
-write.taf(ibts3, "report/ibts3.csv")
+write.taf(ibts3, dir="report")
 
 ## fatage (average, round)
 fatage <- read.taf("output/fatage.csv")
 fatage$"Fbar 2-4" <- rowMeans(fatage[as.character(2:4)])
 fatage <- round(fatage, 3)
-write.taf(fatage, "report/fatage.csv")
+write.taf(fatage, dir="report")
 
 ## natage (sum, round)
 natage <- read.taf("output/natage.csv")
 natage$Total <- rowSums(natage[-1])
 natage <- round(natage)
-write.taf(natage, "report/natage.csv")
+write.taf(natage, dir="report")
 
-## catage_fit (round)
-catage_fit <- read.taf("output/catage_fit.csv")
-catage_fit <- round(catage_fit)
-write.taf(catage_fit, "report/catage_fit.csv")
+## catage.fit (round)
+catage.fit <- read.taf("output/catage_fit.csv")
+catage.fit <- round(catage.fit)
+write.taf(catage.fit, dir="report")
 
 ## summary (trim year, round)
 summary <- read.taf("output/summary.csv")
@@ -129,21 +129,21 @@ names(summary) <- c("Year", "Recruits age 1 ('000)", "Low", "High",
                     "SSB (tons)", "Low", "High",
                     "Total removals (tons)", "Low", "High",
                     "Fbar 2-4", "Low", "High")
-write.taf(summary, "report/summary.csv")
+write.taf(summary, dir="report")
 
-## catch_est (multiplier, round)
-catch_est <- read.taf("output/catch_est.csv")
-names(catch_est)[names(catch_est)=="TotalRemovals"] <- "Total Removals"
-catch_est$"Catch multiplier" <- catch_est$"Total Removals" / catch_est$Catch
-catch_est$"Catch multiplier"[catch_est$"Catch multiplier"==1] <- NA
-catch_est <- catch_est[c("Year","Landings","Discards","Catch",
+## catch.est (multiplier, round)
+catch.est <- read.taf("output/catch_est.csv")
+names(catch.est)[names(catch.est)=="TotalRemovals"] <- "Total Removals"
+catch.est$"Catch multiplier" <- catch.est$"Total Removals" / catch.est$Catch
+catch.est$"Catch multiplier"[catch.est$"Catch multiplier"==1] <- NA
+catch.est <- catch.est[c("Year","Landings","Discards","Catch",
                          "Catch multiplier","Total Removals")]
-catch_est <- rnd(catch_est, c("Landings","Discards","Catch","Total Removals"))
-catch_est <- rnd(catch_est, "Catch multiplier", 2)
-write.taf(catch_est, "report/catch_est.csv")
+catch.est <- rnd(catch.est, c("Landings","Discards","Catch","Total Removals"))
+catch.est <- rnd(catch.est, "Catch multiplier", 2)
+write.taf(catch.est, dir="report")
 
 ## multiplier (round)
 multiplier <- read.taf("output/multiplier.csv")
 names(multiplier)[names(multiplier)=="CatchMultiplier"] <- "Catch multiplier"
 multiplier <- round(multiplier, 2)
-write.taf(multiplier, "report/multiplier.csv")
+write.taf(multiplier, dir="report")
